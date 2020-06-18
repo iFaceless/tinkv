f757bd8e552b903605a992213a8c274fcecc27b6
0xE8551CCB <limeng01@zhihu.com>
2020-06-18 17:53:01 +0800

    🚀 chore: update example

--- src/bin/tinkv.rs
+++ src/bin/tinkv.rs
@@ -6,39 +6,39 @@ use tinkv::{self, Store};
 fn main() -> tinkv::Result<()> {
     pretty_env_logger::init_timed();
     let mut store = Store::open(".tinkv")?;
-    // for i in 0..10 {
-    //     let k = format!("key_{}", i);
-    //     let v = format!(
-    //         "value_{}_{}_the_remove command itself can be deleted in the next compaction",
-    //         i,
-    //         tinkv::util::current_timestamp()
-    //     );
-    //     store.set(k.as_bytes(), v.as_bytes())?;
-    //     store.set(k.as_bytes(), v.as_bytes())?;
-    // }
+    for i in 0..10 {
+        let k = format!("key_{}", i);
+        let v = format!(
+            "value_{}_{}_the_remove command itself can be deleted in the next compaction",
+            i,
+            tinkv::util::current_timestamp()
+        );
+        store.set(k.as_bytes(), v.as_bytes())?;
+        store.set(k.as_bytes(), v.as_bytes())?;
+    }
 
     println!("initial: {:?}", store.stats());
 
-    // let v = store.get("key_1".as_bytes())?.unwrap_or_default();
-    // println!("key_1 => {:?}", String::from_utf8_lossy(&v));
+    let v = store.get("key_1".as_bytes())?.unwrap_or_default();
+    println!("key_1 => {:?}", String::from_utf8_lossy(&v));
 
-    // store.set("hello".as_bytes(), "tinkv".as_bytes())?;
-    // println!("after set 1: {:?}", store.stats());
+    store.set("hello".as_bytes(), "tinkv".as_bytes())?;
+    println!("after set 1: {:?}", store.stats());
 
-    // store.set("hello".as_bytes(), "tinkv 2".as_bytes())?;
-    // println!("after set 2: {:?}", store.stats());
+    store.set("hello".as_bytes(), "tinkv 2".as_bytes())?;
+    println!("after set 2: {:?}", store.stats());
 
-    // store.set("hello 2".as_bytes(), "tinkv".as_bytes())?;
-    // println!("after set 3: {:?}", store.stats());
+    store.set("hello 2".as_bytes(), "tinkv".as_bytes())?;
+    println!("after set 3: {:?}", store.stats());
 
-    // let value = store.get("hello".as_bytes())?;
-    // assert_eq!(value, Some("tinkv 2".as_bytes().to_vec()));
+    let value = store.get("hello".as_bytes())?;
+    assert_eq!(value, Some("tinkv 2".as_bytes().to_vec()));
 
-    // store.remove("hello".as_bytes())?;
-    // println!("after remove: {:?}", store.stats());
+    store.remove("hello".as_bytes())?;
+    println!("after remove: {:?}", store.stats());
 
-    // let value_not_found = store.get("hello".as_bytes())?;
-    // assert_eq!(value_not_found, None);
+    let value_not_found = store.get("hello".as_bytes())?;
+    assert_eq!(value_not_found, None);
 
     store.compact()?;
     println!("after compaction: {:?}", store.stats());
